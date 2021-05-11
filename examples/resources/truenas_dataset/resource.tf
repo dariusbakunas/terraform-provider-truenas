922f2600bffb841aedaa314e19ec5d7a1e9a72d7
Darius Bakunas-Milanowski <bakunas@gmail.com>
2021-05-10 22:37:49 -0400

    docs: add dataset resource example to docs

---
+++ examples/resources/truenas_dataset/resource.tf
@@ -0,0 +1,29 @@
+resource "truenas_dataset" "test" {
+  pool = "<dataset pool>"
+  name = "<dataset name>"
+  parent = "<optional dataset parent for nested datasets>"
+  comments = "Terraform created dataset"
+  compression = "off"
+  sync = "standard"
+  atime = "off"
+  copies = 2
+  quota_bytes = 2147483648
+  quota_critical = 90
+  quota_warning = 70
+  ref_quota_bytes = 1073741824
+  ref_quota_critical = 90
+  ref_quota_warning = 70
+  deduplication = "off"
+  exec = "on"
+  snap_dir = "hidden"
+  readonly = "off"
+  record_size = "256K"
+  case_sensitivity = "mixed"
+
+  inherit_encryption = false
+  encrypted = true
+
+  generate_key = false
+  encryption_algorithm = "AES-128-CCM"
+  encryption_key = "3e10193aa02f4167edc46c9f4b8ba723eed474deede646fded99628de1878d51"
+}
